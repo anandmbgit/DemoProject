808ab081f2a91705859b8e53d50bf80749f5e80b
anandmbgit <anandmb@gmail.com>
2019-07-27 14:00:02 +0530

    new update

---
+++ src/main/java/testDockerDemo/DDemo.java
@@ -0,0 +1,11 @@
+package testDockerDemo;
+
+public class DDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a demo project for docker integration with Jenkins \n");
+
+	}
+
+}
